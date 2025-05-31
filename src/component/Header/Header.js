844047758feae31385e92edc9f3af21ac334d70a
James <gbakonjames800@gmail.com>
2025-05-31 12:40:10 +0100

    fix

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -1,28 +1,41 @@
-import React from 'react'
-import style from '../Header/style.css'
-import logo from '../../Assets/Frame50.png'
-import { Link } from 'react-router-dom'
+import React from "react";
+import style from "../Header/style.css";
+import logo from "../../Assets/Frame50.png";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div className='container'>
-{/* {NAV LOGO} */}
-       <img src={logo} alt= "" className='logo' />
-      
-{/* END OF NAV LOGO */}
-    <nav>
-      <ul>
-     <Link to='/section'>  <li><a href=''>CATALOGUE</a></li>  </Link> 
-        <li><a href=''>FASHION</a></li>
-        <li><a href=''>FAVOURITE</a></li>
-        <li><a href=''>LIFESTLE</a></li>
-        <a href=''><button>SIGNUP</button></a>
-        
-      </ul>
-      
-    </nav>
+    <div className="container">
+      {/* {NAV LOGO} */}
+      <div className="sub-container">
+        {" "}
+        <img src={logo} alt="" className="logo" />
+        {/* END OF NAV LOGO */}
+        <nav>
+          <ul>
+            <Link to="/section">
+              {" "}
+              <li>
+                <a href="">CATALOGUE</a>
+              </li>{" "}
+            </Link>
+            <li>
+              <a href="">FASHION</a>
+            </li>
+            <li>
+              <a href="">FAVOURITE</a>
+            </li>
+            <li>
+              <a href="">LIFESTLE</a>
+            </li>
+            <a href="">
+              <button>SIGNUP</button>
+            </a>
+          </ul>
+        </nav>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
